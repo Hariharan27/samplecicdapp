74fd9321b4307fa2785dd3ce2fbf766f3dbab6bb
Hariharan <hariharan@ideas2it.com>
2020-08-18 14:13:43 +0530

    FEAT: initial project

---
+++ app/src/main/java/com/ideas2it/samplecicdapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ideas2it.samplecicdapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
